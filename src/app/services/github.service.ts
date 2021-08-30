77f696523857305568b09fe0a158236e7944be46
Kalel Chaves <kalel.branco@gmail.com>
2021-08-29 23:22:42 -0300

    Feacture config token

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { environment } from './../../environments/environment';
 
 @Injectable()
 export class GitHubService{
 
     url:string = 'https://api.github.com';    
-    token:string='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'    
+    token:string= environment.gitHubToken;
 
     constructor(private httpClient:HttpClient){
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  gitHubToken: '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'
 };
